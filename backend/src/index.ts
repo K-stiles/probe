a735a54d36f18706a77214ab4e176bd83eb6cc60
K-stiles <michealessuman08@gmail.com>
2025-11-17 01:08:36 +0000

    fix: fixed api entry endpoit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -50,13 +50,9 @@ app.use(
 
 app.get(
   `/`,
-  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException(
-      "This is a bad request",
-      ErrorCodeEnum.AUTH_INVALID_TOKEN
-    );
+  asyncHandler(async (_req: Request, res: Response) => {
     return res.status(HTTPSTATUS.OK).json({
-      message: "Hello Subscribe to the channel & share",
+      message: "👽 Probe API is running...",
     });
   })
 );
